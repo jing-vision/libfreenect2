7ddda2fbfe460f696aa8a362f4a12e08cb1241ce
vinjn <vinjn.z@gmail.com>
2017-01-07 19:19:47 +0800

    Add build_vs2013_x64.bat

---
+++ build_vs2013_x64.bat
@@ -0,0 +1,4 @@
+rmdir /S /Q vs2013_x64
+mkdir vs2013_x64 && cd vs2013_x64
+cmake .. -G "Visual Studio 12 2013 Win64" -DENABLE_OPENCL=OFF -DENABLE_OPENGL=OFF -DENABLE_CUDA=OFF
+cmake --build . --config RelWithDebInfo --target install
